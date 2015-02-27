27ccefd62568df73315a9b4d80facc8a7194a00a
Gerard Turmo <gturmo@gmail.com>
2015-02-27 22:10:38 +0100

    1502272210

--- main.lua
+++ main.lua
@@ -1,3 +1,12 @@
+--Cargar datos caso
+name = display.newText( "María", 200, 50, native.systemFont, 16 )
+name:setFillColor (255, 255, 255)
+local myText = display.newText( "Hello World", 0, 0, native.systemFont, 16 )
+name.anchorX = 0
+name.x = 20
+name.y = 20
+
+
 -- Cargar imagen caso
-local image01 = display.newImage( "DLC_001/images/baby_girl.png", 10, 10, true)
+local image01 = display.newImage( "DLC_001/images/baby_girl.png", 10, 80, true)
 -- Parámetros constantes
