027804cecda9ed067ee558eab720a993742154e4
Gerard Turmo <gturmo@gmail.com>
2015-02-28 19:22:48 +0100

    150228_1922

--- main.lua
+++ main.lua
@@ -1,10 +1,7 @@
 --Cargar datos caso
-name = display.newText( "María", 200, 50, native.systemFont, 16 )
-name:setFillColor (255, 255, 255)
-local myText = display.newText( "Hello World", 0, 0, native.systemFont, 16 )
-name.anchorX = 0
-name.x = 20
-name.y = 20
+name = display.newText( "Marian", 200, 50, native.systemFont, 16 )
+name.x = (60)
+
 
 
 -- Cargar imagen caso
