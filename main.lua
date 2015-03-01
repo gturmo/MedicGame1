2d0375022d2e99a6620eb5111e6e06d128d391db
lluisalmerich <lluisalmerich@hotmail.com>
2015-03-01 11:15:36 +0100

    +
    +

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+--Cargar datos caso
+name = display.newText( "Marian", 200, 50, native.systemFont, 16 )
+name.x = (60)
+
+
+
 -- Cargar imagen caso
+<<<<<<< HEAD
 local image01 = display.newImage( "DLC_001/images/baby_girl.png", 10, 10,  true)
+=======
+local image01 = display.newImage( "DLC_001/images/baby_girl.png", 10, 80, true)
+>>>>>>> origin/master
 -- Parámetros constantes
