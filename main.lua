d1402cd0d9bcd4c8ebe8af000468ea5ad96a5466
lluisalmerich <lluisalmerich@hotmail.com>
2015-03-01 11:14:03 +0100

    33

--- main.lua
+++ main.lua
@@ -1,3 +1,3 @@
 -- Cargar imagen caso
-local image01 = display.newImage( "DLC_001/images/baby_girl.png", 10, 10, true)
+local image01 = display.newImage( "DLC_001/images/baby_girl.png", 10, 10,  true)
 -- Parámetros constantes
